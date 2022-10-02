96c94bcf7d4041ff3f8f51ab45d6c9ea435892b6
darryl west <dpw@raincitysoftware.com>
2022-10-02 13:15:51 -0700

    base62 cli

--- src/bin/base62.rs
+++ src/bin/base62.rs
@@ -1,44 +1,58 @@
-///
-/// Base62 encode and decode
-/// 
-/// 
+//!
+//! Base62 encode and decode
+//!
+//! A command line utility to encode a u64 number to a base62 string with characters [0..9A..Za..z] or
+//! to decode a base62 string to a u64 number.
+//!
 
 use clap::Parser;
 use domain_keys::base62::Base62;
 
-#[derive(Debug, Parser)]
-#[clap(author, version, about, long_about = None)]
-pub struct Config {
+#[derive(Debug, Default, Parser)]
+#[clap(name = "base62")]
+#[command(author)]
+#[clap(version = "1.3")]
+#[clap(long_about = None)]
+#[clap(about = "base62\n\nEncode a u64 number to base62, or decode a base62 String to u64.")]
+pub struct CliArgs {
+    /// encodes a u64 and outputs the string. `base62 -e 12345` -> 3d7
     #[clap(short, long, value_parser, value_name = "u64_number")]
     pub encode: Option<u64>,
 
+    /// decodes a base62 encoded string [0..9A..Za..z] and outputs the integer.  `base62 -d 3d7` -> 12345
     #[clap(short, long, value_parser, value_name = "base62_string")]
     pub decode: Option<String>,
+
+    /// encodes the current system UTC nano-second to base62
+    #[clap(short, long, value_parser)]
+    pub timestamp: bool,
 
     #[clap(short, long, value_parser)]
     pub quiet: bool,
 }
 
-impl Config {
+impl CliArgs {
     pub fn new() -> Self {
-        let config = Config::parse();
-
-        config
+        Self::parse()
     }
 }
 
 fn main() {
-    let config = Config::new();
+    let args = CliArgs::new();
 
-    if let Some(n) = config.encode {
+    println!("{:?}", args);
+
+    if let Some(n) = args.encode {
         println!("{}", Base62::encode(n));
-    } else if let Some(s) = config.decode {
+    } else if let Some(s) = args.decode {
         match Base62::decode(&s) {
             Ok(n) => println!("{}", n),
             Err(err) => eprintln!("Error parsing Base62 string: {:?}", err),
         }
+    } else if args.timestamp {
+        let now = domain_keys::keys::Keys::now() as u64;
+        println!("{} -> {}", now, Base62::encode(now));
     } else {
-        println!("\nError: must add switch to --encode or --decode; see --help\n");
+        println!("\nError: must add switch to --encode or --decode; try base62 --help\n");
     }
-
 }
