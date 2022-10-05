b78fafcb1aa870596550101aad97741d963bfed7
darryl west <dpw@raincitysoftware.com>
2022-10-05 08:27:06 -0700

    integration tests for txkey

--- src/keys.rs
+++ src/keys.rs
@@ -237,13 +237,16 @@ mod tx_key_tests {
 
     #[test]
     fn check_unique() {
-        let mut table = HashSet::new();
-        for _ in 0..1_000 {
+        let max_tests: usize = 1_000;
+        let mut table = HashSet::with_capacity(max_tests);
+        for _ in 0..max_tests {
             let key = TxKey::create();
 
             assert_eq!(key.len(), 12);
             assert_eq!(table.insert(key), true);
         }
+
+        assert_eq!(table.len(), max_tests);
     }
 
     #[test]
