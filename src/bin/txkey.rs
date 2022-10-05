41a98d8c6dc70247f5d61c137cb8d24d269eac3b
darryl west <dpw@raincitysoftware.com>
2022-10-05 08:14:07 -0700

    implemented txkey with unit tests

--- src/bin/txkey.rs
+++ src/bin/txkey.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+use domain_keys::keys::TxKey;
+
+#[derive(Debug, Default, Parser)]
+#[clap(name = "txkey")]
+#[command(author)]
+#[clap(version = "1.3")]
+#[clap(long_about = None)]
+#[clap(about = "txkey\n\ngenerate a new txkey and write the key and timestamp to stdout.")]
+pub struct CliArgs {
+    /// set quiet to suppress the timestamp and show only the key
+    #[clap(short, long, value_parser)]
+    pub quiet: bool,
+}
+
 fn main() {
-    println!("implement me!");
+    let args = CliArgs::parse();
+
+    let key = TxKey::create();
+
+    if args.quiet {
+        println!("{}", key);
+    } else {
+        println!("Key: {}, TimeStamp: {}", key, 0);
+    }
 }
