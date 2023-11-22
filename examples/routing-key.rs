afd1411bdd5800b71688f29d2c862c63011dd8e9
darryl west <darryl.west@raincitysoftware.com>
2023-11-21 17:28:55 -0800

    updated domain keys example to show how routing works

--- examples/routing-key.rs
+++ examples/routing-key.rs
@@ -1,12 +1,12 @@
 use domain_keys::keys::RouteKey;
 
 fn main() {
-    let total_routes = 12;
+    let total_routes = 8;
     println!("Total Routes: {}", total_routes);
-    for _ in 0..total_routes {
+    for _ in 0..50 {
         let key = RouteKey::create();
         if let Ok(route) = RouteKey::parse_route(&key, total_routes) {
-            println!("key: {} - route: {}", key, route);
+            println!("route: {} key: {}", route, key);
         } else {
             panic!("broken key: {}", key);
         }
